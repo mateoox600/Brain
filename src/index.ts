95172fb3661739fced2b8add79bd109c460ffe4b
mateoox600 <matboi862@gmail.com>
2022-05-20 16:55:43 +0200

    Adding check for argv to at least have the file to launch

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ const brain = Brain.getInstance();
 
 const argv = minimist(process.argv.splice(2));
 
+if(argv._.length < 1) throw new Error('Missing file to launch');
+
+
 const file = brain.loadFile(argv._.shift() as string);
 const lexed = brain.lex(file);
 const parsed = brain.parse(lexed);
